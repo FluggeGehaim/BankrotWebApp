ebce837e97b6c0017f5b0b79e8601d684f0477e3
artem_babitski <temacaxp@gmail.com>
2025-04-04 18:05:42 +0300

    catalog filters

--- goods/views.py
+++ goods/views.py
@@ -5,17 +5,26 @@ from goods.models import Lots
 
 def catalog(request, category_slug):
     page = request.GET.get("page", 1)
+    # on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
     
     if category_slug == "all":
         lots = Lots.objects.all()
     else:
         lots = get_list_or_404(Lots.objects.filter(category__slug=category_slug))
+        
+    # if on_sale:
+    #     lots = lots.filter(discount__gt=0)
+    if order_by:
+        lots = lots.order_by(order_by)
+        
 
     paginator = Paginator(lots,9)
     current_page = paginator.page(int(page))
     context = {
         "pagename": "BankrotСпец - Каталог",
         "goods": current_page,
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
