453c4e4417e78f17457b2d256c891e2cecabc052
Artem Babitskii <123790775+FluggeGehaim@users.noreply.github.com>
2025-05-13 03:00:38 +0300

    feat/add_cart (#6)
    * full text search
    
    * add users templates
    
    * registration and logout
    
    * add profile func.
    
    * add cart app
    
    * feat/add_cart

--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render, get_list_or_404
+
+from carts.models import Cart
+
 from users.form import UserLoginForm, UserRegistrationForm, ProfileForm
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
@@ -13,9 +16,20 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+            
+            session_key = request.session.session_key
+            
             if user:
                 auth.login(request, user)
                 messages.success(request, f" {username}, вы успешно авторизовались.")
+                
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
+                    
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page !=  reverse('user:logout'):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -32,8 +46,16 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            
+            session_key = request.session.session_key
+            
             user = form.instance
             auth.login(request, user)
+            
+            if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
+                    
+
             messages.success(request, f" {user.username}, вы успешно зарегестрировались и авторизовались.")
             return HttpResponseRedirect(reverse('main:index'))
     else:
@@ -67,3 +89,7 @@ def logout(request):
     messages.success(request, f"{request.user.username}, вы успешно вышли из аккаунта.")
     auth.logout(request)
     return redirect(reverse('main:index'))
+
+def users_cart(request):
+    return render(request, 'users/users-cart.html')
+
